29dc5bbf009b554265ef1a09d5c81d78c670855d
Gabrielle David <gtdavid@cpp.edu>
2023-04-30 19:44:46 -0700

    Initial files for chat program

---
+++ UDPClient.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.*;
+
+public class UDPClient {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ UDPServer.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.*;
+
+public class UDPServer {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
