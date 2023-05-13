25e9a6563977d66e4c9950e8b64100b051fda7a5
email <krista5055@yahoo.com>
2023-05-12 23:11:08 -0700

    Update UDPClient.java
    added comments to UDPClient

--- UDPClient.java
+++ UDPClient.java
@@ -10,7 +10,7 @@ public class UDPClient {
   private int serverPort; // port # of server we're communicating with 
   private ArrayList<String> peerInfo;
 
-  // takes IP address and port # of sserver
+  // takes IP address and port # of server
   public UDPClient(String serverHost, int serverPort) throws SocketException, UnknownHostException {
     this.socket = new DatagramSocket(); // create new datagram socket 
     this.serverAddress = InetAddress.getByName(serverHost); // convert server hostname to IP address
@@ -21,16 +21,18 @@ public class UDPClient {
   public void send(String message) throws IOException {
     byte[] buffer = message.getBytes(); // convert message to byte array
 
+    // sends message to each peer in peerInfo list
     for (String peer : peerInfo) {
       String[] p = peer.split(" ");
       
-      serverAddress = InetAddress.getByName(p[0]);
-      serverPort = Integer.parseInt(p[1]);
+      serverAddress = InetAddress.getByName(p[0]); // update serverAddress to peer's IP address
+      serverPort = Integer.parseInt(p[1]); //update serverPort to peer's port #
       DatagramPacket packet = new DatagramPacket(buffer, buffer.length, serverAddress, serverPort);
-      socket.send(packet);
+      socket.send(packet);// send packet through socket
     }
   }
 
+  // method sends message to server tracker
   public void sendToTracker(String message, InetAddress address, int port) throws IOException {
     byte[] buffer = message.getBytes(); // convert message to byte array
     DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
@@ -45,21 +47,23 @@ public class UDPClient {
     String message = new String(packet.getData());
 
     if (message.startsWith("peer info")) {
-      getPeerInfo(message);
+      getPeerInfo(message); // update peerInfo list if message contains peer info 
     }
     else {
-      System.out.println(message);
+      System.out.println(message); // else, print message to console
     }
   }
 
+  // method to extract peer info from message 
   public void getPeerInfo(String message) throws IOException{
     peerInfo = new ArrayList<>();
     String[] peers = message.split("\n");
     for(int i = 1; i < peers.length - 1; i++) {
-      peerInfo.add(peers[i]);
+      peerInfo.add(peers[i]); // add each peer to peerInfo list
     }
   }
 
+  // method to run chat session
   public void runChat() throws IOException {
     System.out.println("Welcome to the Chat Room!"); //client prompts a welcome message 
     System.out.print("Please enter your username: "); // asks enter username using the keyboard
@@ -71,6 +75,7 @@ public class UDPClient {
 
     System.out.println("Enter messages (type '.' to exit)"); //to exit the chat, user enters "."
 
+    // create new thread to receive messages from server
     new Thread(() -> {
       try {
         while (true) {
@@ -85,7 +90,7 @@ public class UDPClient {
       String message = scanner.nextLine();
 
       if (message.equals(".")) {
-        sendToTracker(username + " has left the chat.", InetAddress.getByName("localhost"), 5000);
+        sendToTracker(username + " has left the chat.", InetAddress.getByName("localhost"), 5000); // "." indicates that the user has left the chat
         break;
       }
 
@@ -103,7 +108,7 @@ public class UDPClient {
 
   public String getTimestamp() {
     Calendar calendar = Calendar.getInstance();
-    SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+    SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss"); // display time stamp of message
     return format.format(calendar.getTime());
   }
 
@@ -121,7 +126,7 @@ public class UDPClient {
 
         // close the client socket
         client.close();
-        System.out.println("You have left the chat.");
+        System.out.println("You have left the chat."); // message to display user has left the chat
         System.exit(0);
     } catch (IOException e) {
         e.printStackTrace();
